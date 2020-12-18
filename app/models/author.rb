836f8263ba90e2347e4c42c9c0cb7b45287768a6
Rmiverson <Rmiverson99@gmail.com>
2020-12-18 09:46:40 -0800

    Mission Accomplished

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ActiveRecord::Base
+   validates_presence_of :name
+   validates_length_of :phone_number, :is => 10
+   validates_uniqueness_of :name
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
+require'pry'
+
 class Post < ActiveRecord::Base
+   validates_presence_of :title
+   validates_length_of :content, :minimum => 250
+   validates_length_of :summary, :maximum => 250
+   validates :category, inclusion: %w(Fiction Non-Fiction)
+   validate :is_clickbait_valid
+   #   binding.pry 
+
+   CLICKBAIT = ["Won't Believe", "Secret", "Top[number]", "Guess"]
+
+   def is_clickbait_valid
+      if title.present? && CLICKBAIT.none? { |z| title.match(z)}
+         errors.add(:title, "doesnt sound clickbait-y enough")
+      end
+   end
 end
